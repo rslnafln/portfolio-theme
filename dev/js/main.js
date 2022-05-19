fd57315cf44102596ac8c978ca6755a6b823dae6
emilebeguin <milleetunenuit@gmail.com>
2022-05-19 16:13:48 +0200

    couleurs des sections dans le HTML

---
+++ dev/js/main.js
@@ -0,0 +1,4 @@
+let navBar = document.querySelector('header nav.nav');
+let sectionsArray = document.getElementsByTagName('section');
+
+// la nav prend la classe de la section à chaque fois qu'elle passe dessus
