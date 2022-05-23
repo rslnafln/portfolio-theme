bb89b73ceedf18ec4ce47bbec5c28c6bfef7c145
Émile Beguin <milleetunenuit@gmail.com>
2022-05-23 12:57:01 +0200

    timing swiper, quelques animations sur le contact form

--- dev/js/swiper.js
+++ dev/js/swiper.js
@@ -4,5 +4,8 @@ let swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
+    },
+    autoplay: {
+        delay: 5000,
+    },
 });
